267c196b3358a4da644db5db10ed1a17df03f550
mcaloger <mattcaloger@gmail.com>
2022-04-19 23:08:39 -0400

    Initial

---
+++ Books/Controller/BookController.cs
@@ -0,0 +1,49 @@
+﻿using BookDB.Books.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDB.Books
+{
+    [Route("api/v1/book")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService bookService;
+
+        public BookController(BookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        [HttpGet("/")]
+        public ActionResult<IEnumerable<BookModel>> GetBooks()
+        {
+            return bookService.GetAllBooks();
+        }
+
+        [HttpGet("/{id}")]
+        public ActionResult<BookModel> GetBook(int id)
+        {
+            return bookService.GetBook(id);
+        }
+
+        [HttpPost("/create")]
+        public ActionResult<BookModel> CreateBook(BookModel bookModel)
+        {
+            return bookService.CreateBook(bookModel);
+        }
+
+        [HttpPost("/update")]
+        public ActionResult<BookModel> UpdateBook(BookModel bookModel)
+        {
+            return bookService.UpdateBook(bookModel);
+        }
+
+        [HttpPost("/delete")]
+        public ActionResult DeleteBook(BookModel bookModel)
+        {
+            bookService.DeleteBook(bookModel);
+            return Ok();
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BookDB.Books;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookDB.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<BookModel> Books { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
